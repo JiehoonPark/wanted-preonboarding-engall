59ac1f47975c5a37e0c1b8ca9acf6dd968572a14
mirim <alfla6528@naver.com>
2022-07-28 01:58:49 +0900

    feat: global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@styles/GlobalStyle';
+import theme from '@styles/theme';
 
 function App() {
-  return <div>helloworld!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const color = {
+  grey_01: '#F4F4F4',
+  grey_02: '#EFEFEF',
+  grey_03: '#E9E9E9',
+  grey_04: '#B4B4B4',
+  grey_05: '#959595',
+  blue_01: '#3175D8',
+  blue_02: '#44A7C8',
+};
+
+const theme = {
+  color,
+};
+
+export default theme;
