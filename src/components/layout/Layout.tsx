22ec9fd9b8ebb2a060373fe2b1b10f97d8e925ff
mirim <alfla6528@naver.com>
2022-07-28 03:11:56 +0900

    feat: add layout to main

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import styled from 'styled-components';
 
 export default function Layout() {
-  return <div>Layout</div>;
+  return (
+    <LayoutContainer>
+      <Header>
+        <Logo />
+      </Header>
+      <Main>
+        <Outlet />
+      </Main>
+    </LayoutContainer>
+  );
 }
+
+const LayoutContainer = styled.div``;
+const Header = styled.div`
+  height: 80px;
+  background: ${({ theme }) => theme.color.blue_02};
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    height: 60px;
+  }
+`;
+const Logo = styled.img.attrs({
+  src: './images/logo.png',
+})`
+  width: 100px;
+  height: 55px;
+  margin: 20px 0 0 40px;
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    width: 75px;
+    height: 42px;
+    margin: 10px 0 0 20px;
+  }
+`;
+const Main = styled.main``;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,12 +3,15 @@ import { Route, Routes } from 'react-router';
 
 import WeeklySchedule from '@pages/WeeklySchedule';
 import Edit from '@pages/Edit';
+import Layout from '@components/layout/Layout';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<WeeklySchedule />} />
-      <Route path="/edit" element={<Edit />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<WeeklySchedule />} />
+        <Route path="/edit" element={<Edit />} />
+      </Route>
     </Routes>
   );
 }
