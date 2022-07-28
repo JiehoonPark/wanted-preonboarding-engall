d0f55f1a0e738caeb3f309df7f5e75717e1a2036
mirim <alfla6528@naver.com>
2022-07-28 19:59:03 +0900

    feat: Implement ScheduleForm component

--- src/pages/ScheduleForm.tsx
+++ src/pages/ScheduleForm.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import Button from '@src/components/common/Button';
+import PageBox from '@src/components/common/PageBox';
+import Title from '@src/components/common/Title';
+import StartTime from '@src/components/form/StartTime';
+import WeekCheckBox from '@src/components/form/WeekCheckBox';
+
+import styled from 'styled-components';
 
 function ScheduleForm() {
-  return <div>ScheduleForm</div>;
+  return (
+    <ScheduleFormContainer>
+      <TitleWrap>
+        <Title>Add class schedule</Title>
+      </TitleWrap>
+      <PageBox>
+        <StartTime />
+        <WeekCheckBox />
+      </PageBox>
+      <ButtonWrap>
+        <Button>Save</Button>
+      </ButtonWrap>
+    </ScheduleFormContainer>
+  );
 }
 
 export default ScheduleForm;
+
+const ScheduleFormContainer = styled.div``;
+
+const TitleWrap = styled.div`
+  margin-bottom: 50px;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: end;
+  margin-top: 35px;
+`;
