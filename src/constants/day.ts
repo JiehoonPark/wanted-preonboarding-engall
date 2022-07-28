a38ce9a52eb83307583f5ce3ac3a8285caab2d77
mirim <alfla6528@naver.com>
2022-07-28 19:52:54 +0900

    feat: add checkbox array

--- src/constants/day.ts
+++ src/constants/day.ts
@@ -6,6 +6,14 @@ const FRIDAY = 'Friday';
 const SATURDAY = 'Saturday';
 const SUNDAY = 'Sunday';
 
+const Mon = 'Mon';
+const Tue = 'Tue';
+const Wed = 'Wed';
+const Thu = 'Thu';
+const Fri = 'Fri';
+const Sat = 'Sat';
+const Sun = 'Sun';
+
 export const week = [
   MONDAY,
   TUESDAY,
@@ -14,4 +22,14 @@ export const week = [
   FRIDAY,
   SATURDAY,
   SUNDAY,
+];
+
+export const checkboxWeek = [
+  [MONDAY, Mon],
+  [TUESDAY, Tue],
+  [WEDNESDAY, Wed],
+  [THURSDAY, Thu],
+  [FRIDAY, Fri],
+  [SATURDAY, Sat],
+  [SUNDAY, Sun],
 ];
