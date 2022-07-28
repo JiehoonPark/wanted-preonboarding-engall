05cd186a3eb4b16f473985eaaddbf11c2151fa91
mirim <alfla6528@naver.com>
2022-07-28 15:30:32 +0900

    feat: 문자열 상수화

---
+++ src/constants/day.ts
@@ -0,0 +1,17 @@
+const MONDAY = 'Monday';
+const TUESDAY = 'Tuesday';
+const WEDNESDAY = 'Wednesday';
+const THURSDAY = 'Thursday';
+const FRIDAY = 'Friday';
+const SATURDAY = 'Saturday';
+const SUNDAY = 'Sunday';
+
+export const week = [
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+];
