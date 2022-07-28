73ed9791c4b315ebc1e115dbb71226e49c0d750f
mirim <alfla6528@naver.com>
2022-07-28 15:39:28 +0900

    feat: add theme color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -10,8 +10,10 @@ const color = {
   grey_01: '#F4F4F4',
   grey_02: '#EFEFEF',
   grey_03: '#E9E9E9',
-  grey_04: '#B4B4B4',
-  grey_05: '#959595',
+  grey_04: '#CCCCCC',
+  grey_05: '#B4B4B4',
+  grey_06: '#959595',
+  grey_07: '#747474',
   blue_01: '#3175D8',
   blue_02: '#44A7C8',
 };
