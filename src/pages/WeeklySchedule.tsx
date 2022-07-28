3f2f00860c9aa7f38b36937f8f8aedfd3dc4cd70
mirim <alfla6528@naver.com>
2022-07-28 19:59:26 +0900

    feat: Implement WeeklySchedule component

--- src/pages/WeeklySchedule.tsx
+++ src/pages/WeeklySchedule.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Title from '@components/common/Title';
+import Button from '@components/common/Button';
+import ScheduleDayBox from '@src/components/schedule/ScheduleDayBox';
+import ScheduleWeeklyBox from '@src/components/schedule/ScheduleWeeklyBox';
 
 function WeeklySchedule() {
-  return <div>WeeklySchedule</div>;
+  return (
+    <WeeklyScheduleContainer>
+      <Wrap>
+        <Title>Class schedule</Title>
+        <ButtonWrap>
+          <Button>Add Class Schedule</Button>
+        </ButtonWrap>
+      </Wrap>
+      <Box>
+        <ScheduleWeeklyBox />
+        <ScheduleDayBox />
+      </Box>
+    </WeeklyScheduleContainer>
+  );
 }
 
 export default WeeklySchedule;
+
+const WeeklyScheduleContainer = styled.div``;
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 50px;
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    display: block;
+    margin-bottom: 30px;
+  }
+`;
+const ButtonWrap = styled.div`
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    display: flex;
+    justify-content: end;
+    margin: 50px 0 30px;
+  }
+`;
+
+const Box = styled.div`
+  .weekly {
+    display: block;
+  }
+  .day {
+    display: none;
+  }
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    .weekly {
+      display: none;
+    }
+    .day {
+      display: block;
+    }
+  }
+`;
