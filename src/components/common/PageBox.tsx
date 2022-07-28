4cae923a2f450c56446ae36543543fb13383de3c
mirim <alfla6528@naver.com>
2022-07-28 19:57:08 +0900

    feat: Implement PageBox components

---
+++ src/components/common/PageBox.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface IPageBoxProps {
+  children: ReactNode;
+}
+
+function PageBox({ children }: IPageBoxProps) {
+  return <PageBoxContainer>{children}</PageBoxContainer>;
+}
+
+export default PageBox;
+
+const PageBoxContainer = styled.div`
+  width: 100%;
+  min-height: 340px;
+  background: #ffffff;
+  box-shadow: 0px 4px 3px rgba(0, 0, 0, 0.25);
+  padding: 25px 20px;
+`;
