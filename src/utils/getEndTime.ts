dd168bbcc34b0b4f7b7b129132d05266501742bd
mirim <alfla6528@naver.com>
2022-07-29 19:25:03 +0900

    remove: remove file

--- src/utils/getEndTime.ts
+++
@@ -1,4 +0,0 @@
-const getEndTime = (start: number[]) =>
-  start[1] >= 20 ? [start[0] + 1, start[1] - 20] : [start[0], start[1] + 40];
-
-export default getEndTime;
--- src/utils/setTimeFormat.ts
+++
@@ -1,4 +0,0 @@
-const setTimeFormat = (data: number[]) =>
-  data.map(time => (time < 10 ? `0${time}` : time)).join(':');
-
-export default setTimeFormat;
