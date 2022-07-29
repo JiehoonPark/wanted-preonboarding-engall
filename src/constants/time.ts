c864d65cb09c59ee10f982cd7871335b52693937
mirim <alfla6528@naver.com>
2022-07-29 19:24:08 +0900

    feat: add number options

--- src/constants/time.ts
+++ src/constants/time.ts
@@ -1,14 +1,18 @@
 export const AM = 'AM';
 export const PM = 'PM';
 
-export const hours = Array(12)
-  .fill(1)
-  .map((hour, index) =>
-    hour * index < 10 ? `0${hour * index}` : `${hour * index}`,
-  );
+export const BASE_ON_DATE = [2022, 7, 1];
 
-export const minutes = Array(12)
+export const hour = Array(12)
+  .fill(1)
+  .map((hour, index) => {
+    const time = hour * index;
+    return [time, time < 10 ? `0${time}` : `${time}`];
+  });
+
+export const minute = Array(12)
   .fill(5)
-  .map((minute, index) =>
-    index === 0 || index === 1 ? `0${minute * index}` : `${minute * index}`,
-  );
+  .map((minute, index) => {
+    const time = minute * index;
+    return [time, index === 0 || index === 1 ? `0${time}` : `${time}`];
+  });
